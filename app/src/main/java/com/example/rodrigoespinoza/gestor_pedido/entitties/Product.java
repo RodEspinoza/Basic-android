468419ca2d2ecd4717ad39169cefa9180c4d77f8
Rodrigo Espinoza <rodrigoespinoza@MacBook-Pro-de-Rodrigo-2.local>
2018-09-01 09:43:08 -0300

    Product

--- app/src/main/java/com/example/rodrigoespinoza/gestor_pedido/entitties/Product.java
+++ app/src/main/java/com/example/rodrigoespinoza/gestor_pedido/entitties/Product.java
@@ -1,4 +1,37 @@
 package com.example.rodrigoespinoza.gestor_pedido.entitties;
 
 public class Product {
+    Integer id;
+    String name;
+    Integer stock;
+
+    public Product(Integer id, String name, Integer stock) {
+        this.id = id;
+        this.name = name;
+        this.stock = stock;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
 }
