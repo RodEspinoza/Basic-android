128517a81186d07859f78543cd93749006ddc749
novacane <espinoza.sample@gmail.com>
2018-09-02 16:07:12 -0300

    no

--- app/src/main/java/com/example/rodrigoespinoza/gestor_pedido/activity_add_products.java
+++ app/src/main/java/com/example/rodrigoespinoza/gestor_pedido/activity_add_products.java
@@ -2,12 +2,25 @@ package com.example.rodrigoespinoza.gestor_pedido;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class activity_add_products extends AppCompatActivity {
-
+public class activity_add_products extends AppCompatActivity implements View.OnClickListener {
+    EditText txProductName, txProductStock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_products);
+        this.txProductName = findViewById(R.id.txProductName);
+        this.txProductStock = findViewById(R.id.txProductStock);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnSubmitNewProduct:
+                break;
+        }
     }
 }
