104014d0759a3fb7ad6164a21cb00cf215168568
Rodrigo Alonso <espinoza.sample@gmail.com>
2018-09-04 17:30:50 -0300

    TODO

--- app/src/main/java/com/example/rodrigoespinoza/gestor_pedido/activity_porders.java
+++ app/src/main/java/com/example/rodrigoespinoza/gestor_pedido/activity_porders.java
@@ -3,12 +3,21 @@ package com.example.rodrigoespinoza.gestor_pedido;
 import android.os.Bundle;
 import android.app.Activity;
 
+Button btnOpenProductsView;
 public class activity_porders extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_porders);
+        //this.btnOpenProductsView = findViewById(R.id.btnOpenProductsView)
+        
     }
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+        }
+    }
+    
 
 }
