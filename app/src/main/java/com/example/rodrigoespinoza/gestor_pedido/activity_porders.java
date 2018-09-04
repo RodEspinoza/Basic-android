cd1a07591ea237f9372bab973b89d88e3970850e
novacane <espinoza.sample@gmail.com>
2018-09-04 00:43:23 -0300

    add init activity

---
+++ app/src/main/java/com/example/rodrigoespinoza/gestor_pedido/activity_porders.java
@@ -0,0 +1,14 @@
+package com.example.rodrigoespinoza.gestor_pedido;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class activity_porders extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_porders);
+    }
+
+}
