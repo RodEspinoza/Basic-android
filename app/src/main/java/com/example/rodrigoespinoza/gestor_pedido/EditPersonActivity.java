3af35709fbe34d6c2ceb571acf95533a774ad1ae
Enzo Soto <enz.soto@gmail.com>
2018-09-11 22:59:07 -0300

    commit

--- app/src/main/java/com/example/rodrigoespinoza/gestor_pedido/EditPersonActivity.java
+++ app/src/main/java/com/example/rodrigoespinoza/gestor_pedido/EditPersonActivity.java
@@ -101,7 +101,7 @@ public class EditPersonActivity extends AppCompatActivity implements View.OnClic
         spLocationEdit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                
+
             }
 
             @Override
